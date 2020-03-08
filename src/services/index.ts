e812940f7cea5a28aaee9fed26854c26da481614
abalasu1 <abalasu1@in.ibm.com>
2020-03-08 17:53:18 +0530

    Adds service implementation

--- src/services/index.ts
+++ src/services/index.ts
@@ -5,3 +5,4 @@ export * from './task.service';
 export * from './stock-items-mock.service';
 export * from './stock-items.api';
 export * from './stock-items-mock.service';
+export * from './stock-items.service';
